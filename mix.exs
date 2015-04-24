c5db9a5e1655efb2529fea170c8b6e9a5cfc2e11
Alexander <alxndr+github@gmail.com>
2015-04-24 00:23:52 -0700

    version tweaks

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule TweetMegaphone.Mixfile do
 
   def project do
     [app: :tweet_megaphone,
-     version: "0.0.1",
-     elixir: "~> 1.0",
+     version: "0.0.0",
+     elixir: "~> 1.0.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
