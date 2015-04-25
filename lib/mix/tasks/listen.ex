5024122e6cb54848655719f1677aeedb315afdcc
Alexander <alxndr+github@gmail.com>
2015-04-25 14:08:01 -0700

    `mix listen` streams tweets

--- lib/mix/tasks/listen.ex
+++ lib/mix/tasks/listen.ex
@@ -9,7 +9,22 @@ defmodule Mix.Tasks.Listen do
   def run([search_term]), do: listen_for(search_term)
 
   defp listen_for(search_term) do
-    IO.inspect search_term
+    IO.puts "listening for: #{search_term}"
+    stream = ExTwitter.stream_filter(track: search_term, receive_messages: true)
+    for message <- stream do
+      case message do
+        tweet = %ExTwitter.Model.Tweet{} ->
+          IO.puts "tweet = #{tweet.text}"
+        deleted_tweet = %ExTwitter.Model.DeletedTweet{} ->
+          IO.puts "deleted tweet = #{deleted_tweet.status[:id]}"
+        limit = %ExTwitter.Model.Limit{} ->
+          IO.puts "limit = #{limit.track}"
+        stall_warning = %ExTwitter.Model.StallWarning{} ->
+          IO.puts "stall warning = #{stall_warning.code}"
+        _ ->
+          IO.inspect message
+      end
+    end
   end
 
 end
